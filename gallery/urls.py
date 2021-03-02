be39f5328374199fe1cd42eaf971ff918c36cd03
Shira <shiraej@gmail.com>
2021-03-01 21:53:39 -0800

    made the page for each album (details.html and started styling with Bootstrap/CSS. Did Navbar, gallery, and details page

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.adv_gallery, name='gallery'),
+    path('<int:adventure_id>/', views.adv_details, name='details')
     #path('entry/', views.guestentry, name = 'guestbook_entry')
 ]
