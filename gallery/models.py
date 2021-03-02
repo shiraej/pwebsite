a200506a4f443b921dbc1fa709b21c0cbf684cc0
Shira <shiraej@gmail.com>
2021-03-01 21:54:52 -0800

    Revert "made the page for each album (details.html and started styling with Bootstrap/CSS. Did Navbar, gallery, and details page"
    This reverts commit be39f5328374199fe1cd42eaf971ff918c36cd03.

--- gallery/models.py
+++ gallery/models.py
@@ -7,12 +7,6 @@ def make_pic_path (instance, filename):
 	advname = instance.adventure.name
 	path = 'AdvPics/{0}/{1}'.format(advname, filename)
 	return path
-
-def make_gps_path (instance, filename):
-	advname = instance.adventure.name
-	path = 'AdvGPS/{0}/{1}'.format(advname, filename)
-	return path
-
 #def cover_photo (instance):
 	#cover = instance.Adventure.objects.filter(adventurepicture__cover = True)
 	#return instance.adventurepicture.objects.(cover = True)
@@ -21,11 +15,10 @@ class Adventure(models.Model):
 	name = models.CharField(max_length = 100)
 	dates = models.CharField(max_length = 100) 
 	description = models.CharField(max_length = 1000)
-	gpstrax = models.FileField(upload_to = make_gps_path, blank = True)
 	
 	def cover_photo (self):
 		cov = self.adventurepicture_set.get(cover = True)
-		return cov.picture
+		return cov.picture_smole
 
 	cover = cover_photo
 	def __str__(self):
--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Adventure, AdventurePicture
 from django.contrib.auth.decorators import login_required
 
@@ -7,14 +7,3 @@ def adv_gallery(request):
 	gallery = Adventure.objects.all()
 	context = {'gallery': gallery, }
 	return render(request, 'gallery/gallery.html', context)
-
-def adv_details(request, adventure_id):
-	adv = get_object_or_404(Adventure, pk=adventure_id)
-	#pics = get_object_or_404(AdventurePicture, pk=adventure_id)
-	try:
-		pics = AdventurePicture.objects.filter(adventure=adventure_id)
-	except AdventurePicture.DoesNotExist:
-		raise Http404("Pictures do not exist")
-	context = {'adv' : adv, 'pics' : pics}
-	return render(request, 'gallery/details.html', context)
-
