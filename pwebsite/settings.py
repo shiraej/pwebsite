2c98770b536ffbfa4019f00143ebdd3cfbd0f7ce
Shira <shiraej@gmail.com>
2021-04-09 11:37:11 -0700

    using environment variables to set database, secret key etc. from .env file.

--- pwebsite/settings.py
+++ pwebsite/settings.py
@@ -92,25 +92,12 @@ WSGI_APPLICATION = 'pwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'pwebsite',
-            'USER': 'shira',
-            'PASSWORD': 'shira',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+if len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
