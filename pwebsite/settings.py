a0bd65a86bfe6e2a4b1cd2886bce4505d45e1a90
Shira <shiraej@gmail.com>
2021-04-11 11:20:54 -0700

    changed settings file to use 'static' as static_root

--- pwebsite/settings.py
+++ pwebsite/settings.py
@@ -43,8 +43,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (BASE_DIR /'static',)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (BASE_DIR /'staticfiles',)
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
